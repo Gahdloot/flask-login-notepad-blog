83b15a00bea73a09ac98218f3bd9d91ac8aca5b4
Michael David <davidlearn27@gmail.com>
2022-08-06 18:06:22 +0100

    commenting in the model file

--- website/models.py
+++ website/models.py
@@ -2,10 +2,14 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
+# the user has a many to one relationship withe the note
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    #user_id is a foreign key in Note
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
